7d71d9b6c32973fe5971ceef6c37a7d9b404227c
teruyo14 <113246015+teruyo14@users.noreply.github.com>
2024-04-12 10:49:48 +0900

    Add files via upload

---
+++ A問題/A25.rs
@@ -0,0 +1,41 @@
+use std::io::stdin;
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let hw: Vec<usize> = input
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mut grid = vec![];
+    for _ in 0..hw[0] {
+        input.clear();
+        stdin().read_line(&mut input).unwrap();
+        grid.push(input.trim().to_string());
+    }
+
+    let mut dp: Vec<Vec<i128>> = vec![vec![0; hw[1]]; hw[0]];
+
+    dp[0][0] = if grid[0].chars().nth(0).unwrap() == '.' {
+        1
+    } else {
+        0
+    };
+
+    for i in 0..hw[0] {
+        for j in 0..hw[1] {
+            if grid[i].chars().nth(j).unwrap() != '#' {
+                if i > 0 {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if j > 0 {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+    }
+
+    println!("{}", dp[hw[0] - 1][hw[1] - 1]);
+}
