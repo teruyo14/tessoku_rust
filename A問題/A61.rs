5ace0565fc1bcc6dda97d86f5f70cd23546f40c9
teruyo14 <113246015+teruyo14@users.noreply.github.com>
2024-04-15 17:32:33 +0900

    Update A61.rs

--- A問題/A61.rs
+++ A問題/A61.rs
@@ -4,13 +4,19 @@ use std::io::stdin;
 fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
-    let nm: Vec<usize> = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let nm: Vec<usize> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
 
-    let mut map = vec![vec![];nm[0]+1];
+    let mut map = vec![vec![]; nm[0] + 1];
     for _ in 0..nm[1] {
         input.clear();
         stdin().read_line(&mut input).unwrap();
-        let ab: Vec<usize> = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        let ab: Vec<usize> = input
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
         map[ab[0]].push(ab[1]);
         map[ab[1]].push(ab[0]);
     }
