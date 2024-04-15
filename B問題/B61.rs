0fd7154db0c66a6afec86baabe49e45e41203eec
teruyo14 <113246015+teruyo14@users.noreply.github.com>
2024-04-15 17:42:02 +0900

    Add files via upload

---
+++ B問題/B61.rs
@@ -0,0 +1,32 @@
+use std::io::stdin;
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let nm: Vec<usize> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mut map = vec![vec![]; nm[0] + 1];
+    for _ in 0..nm[1] {
+        input.clear();
+        stdin().read_line(&mut input).unwrap();
+        let ab: Vec<usize> = input
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        map[ab[0]].push(ab[1]);
+        map[ab[1]].push(ab[0]);
+    }
+
+    let mut ans = 0;
+    let mut cnt = 0;
+    for i in 1..=nm[0] {
+        if cnt < map[i].len() {
+            cnt = map[i].len();
+            ans = i;
+        }
+    }
+    println!("{}", ans);
+}
