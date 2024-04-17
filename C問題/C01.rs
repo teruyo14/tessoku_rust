6fbd8c6f71e9e87509b208fcffe2090775852617
teruyo14 <113246015+teruyo14@users.noreply.github.com>
2024-04-17 22:11:47 +0900

    Add files via upload

---
+++ C問題/C01.rs
@@ -0,0 +1,9 @@
+use std::io::stdin;
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+
+    println!("{:.0}", n as f64 * 1.1);
+}
